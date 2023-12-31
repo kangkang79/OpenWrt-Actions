1622182b79744b2e7aa7711754bd143d27cbd6f1
kangkang79 <133557059+kangkang79@users.noreply.github.com>
2023-12-31 14:12:37 +0800

    Initial for rax3000m actions
    Initial for rax3000m actions

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,7 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main'>>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git OpenClash https://github.com/vernesong/OpenClash;master' >>feeds.conf.default
+
