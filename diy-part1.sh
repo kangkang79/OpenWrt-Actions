804682c6607e23fbaf509622b69f7ff8d331769f
kangkang79 <133557059+kangkang79@users.noreply.github.com>
2023-12-31 14:52:16 +0800

    Use custom feeds and config

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,8 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main'>>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git OpenClash https://github.com/vernesong/OpenClash;master' >>feeds.conf.default
+#echo 'src-git lede_luci https://github.com/coolsnowwolf/luci.git>>feeds.conf.default
+#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main'>>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git OpenClash https://github.com/vernesong/OpenClash;master' >>feeds.conf.default
 
